bf4e7a6c0612acdb7f7ce0e3321eab88dc242d33
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-02-20 23:39:29 +0530

    comments of a particular user on particular post

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -14,6 +14,24 @@ const User = {
         x.visibility === args.visibility.toLowerCase()
     );
   },
+  comments: (
+    parent,
+    args: { idOfRepo: string },
+    { db },
+    info
+  ): [{ object }] => {
+    const repoExists = db.repos.findIndex((x) => x.id === args.idOfRepo);
+
+    if (repoExists === -1) {
+      throw new Error("Repo does not exist");
+    }
+    if (db.repos[repoExists].visibility === "private") {
+      throw new Error("Repo is Private");
+    }
+    return db.comments.filter(
+      (x) => x.developer === parent.id && x.repoId === args.idOfRepo
+    );
+  },
 };
 
 export { User as default };
