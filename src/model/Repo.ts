3905c3139dc9abf237ba856a0d615cd0ace19b64
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-03-07 19:08:43 +0530

    changed Repo Model

--- src/model/Repo.ts
+++ src/model/Repo.ts
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const repoSchema = new Schema({
   title: { type: String, required: true },
   visibility: { type: String, required: true },
+  desc: { type: String, required: true },
   developer: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
