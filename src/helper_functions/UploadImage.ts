d1cc33e285cd59e7136603ce5eaac8b63069d3e0
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-04-16 14:07:43 +0530

    Saving image locally

--- src/helper_functions/UploadImage.ts
+++ src/helper_functions/UploadImage.ts
@@ -1,11 +1,25 @@
-import shortid from 'shortid'
-import {extensionCheck} from "./ExensionCheck"
+import shortid from "shortid";
+import { extensionCheck } from "./ExensionCheck";
+const { createWriteStream, unlinkSync } = require("fs");
+const path = require("path");
 
-export const uploadImage = async(file) => {
+export const uploadImage = async (file) => {
+  const id = shortid.generate();
+  const { createReadStream, filename } = await file;
 
-    const id = shortid.generate()
-    const { createReadStream , filename } = await file;
+  await extensionCheck(filename);
+  const save_path = path.join(__dirname, "../../images", `/${id}-${filename}`);
 
-    await extensionCheck(filename)
-
-}
+  await new Promise((resolve, reject) =>
+    createReadStream()
+      .on("error", (error) => {
+        if (createReadStream().truncated)
+          // delete the truncated file
+          unlinkSync(path);
+        reject(error);
+      })
+      .pipe(createWriteStream(save_path))
+      .on("error", (error) => reject(error))
+      .on("finish", () => resolve({ path }))
+  );
+};
